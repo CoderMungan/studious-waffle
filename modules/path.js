08b588c4b834adf6a779612df57be5484c68cb16
CoderMungan <codermungan@gmail.com>
2024-05-17 01:45:22 +0300

    feat(path-module): add lesson path module

---
+++ modules/path.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+console.log(
+  "Extname-ciktisi:",
+  path.extname("~/Desktop/Eğitim/Nodejs/modules/path.js"),
+); // .js olarak cikti verecektir
+console.log(
+  "Basename-ciktisi:",
+  path.basename("~/Desktop/Eğitim/Nodejs/modules/path.js"),
+); // path olarak cikti verecektir
+console.log(
+  "Dir-ciktisi:",
+  path.dirname("~/Desktop/Eğitim/Nodejs/modules/path.js"),
+); // ~/Desktop/Eğitim/Nodejs/modules/path.js olarak cikti verecektir.
+
+const pathFormat = path.format({
+  root: "/test/coodermungan/",
+  name: "file",
+  ext: ".js",
+});
+
+console.log("format", pathFormat); // /test/coodermungan/file.js seklinde cikti verecektir.
+
+console.log("join", path.join("/local", "test", "run/d", "x")); // \local\test\run\d\x seklinde cikti verecektir.
+
+console.log("parse", path.parse("~/Desktop/Eğitim/Nodejs/modules/path.js"));
+// parse {
+// root: '',
+// dir: '~/Desktop/Eğitim/Nodejs/modules/',
+// base: 'path.js',
+// ext: '.js',
+// name: 'path'
+// }
---
+++ readme.md
@@ -0,0 +1,17 @@
+### Self Learning
+
+## Roadmap
+
+- Modules
+- MVC Architechture
+- Api
+- Middlewares
+- Relational Database (Mysql, MongoDB)
+- ORM Relational DB (Sequilize, Prisma, Mongoose)
+- Validator
+- RabbitMQ
+- PM2
+- Socketio
+- Redis
+- MicroService
+- Dockerize
