9012062a27da37d81acd9343b32382d79df4fe26
CoderMungan <codermungan@gmail.com>
2024-05-25 00:26:06 +0300

    feat(fs): add module some examples

--- modules/fs.js
+++ modules/fs.js
@@ -12,3 +12,28 @@ for (let i = 0; i < 10; i++) {
     console.log("Error Message", err);
   });
 }
+
+// for the file opening
+fs.open("index.txt", "w", (err) => {
+  console.log("File Opened");
+});
+
+// for the file closing
+fs.close(3, (err) => {
+  console.log("File Closed");
+});
+
+// for the file deleting
+fs.unlink("index.txt", (err) => {
+  console.log("File Deleted");
+});
+
+// for the file renaming
+fs.rename("index.txt", "index3.txt", (err) => {
+  console.log("File Renamed");
+});
+
+// for the file reading
+fs.readFile("index2.txt", "utf8", (err, data) => {
+  console.log("file read", data.toString());
+});
