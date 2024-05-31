42c8ccf6524753eb0191ab5accb23673ee2f66a0
CoderMungan <codermungan@gmail.com>
2024-05-31 03:51:21 +0300

    feat(javascript): add some practice algorithm

--- algoritms-pratics.js
+++ algoritms-pratics.js
@@ -1,30 +1,29 @@
-// // factorial
-// function factorial(n) {
-//   if (n <= 0) {
-//     return 1;
-//   }
-//
-//   return n * factorial(n - 1);
-// }
-//
-// console.log("Sonuc:", factorial(5));
-//
-// // fibonacci
-// function fibonacci(n) {
-//   if (n <= 0) return [];
-//   if (n === 1) return [0];
-//
-//   let sequence = [0, 1];
-//
-//   for (let i = 2; i < n; i++) {
-//     sequence.push(sequence[i - 1] + sequence[i - 2]);
-//   }
-//
-//   return sequence;
-// }
-//
-// console.log("Sonuc:", fibonacci(50));
-//
+// factorial
+function factorial(n) {
+  if (n <= 0) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
+}
+
+console.log("Sonuc:", factorial(5));
+
+// fibonacci
+function fibonacci(n) {
+  if (n <= 0) return [];
+  if (n === 1) return [0];
+
+  let sequence = [0, 1];
+
+  for (let i = 2; i < n; i++) {
+    sequence.push(sequence[i - 1] + sequence[i - 2]);
+  }
+
+  return sequence;
+}
+
+console.log("Sonuc:", fibonacci(50));
 
 // array sum
 function arraySum(arr) {
