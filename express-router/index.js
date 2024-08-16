45012b3f7730100cca20d753825b2d2af27689f6
CoderMungan <codermungan@gmail.com>
2024-08-17 02:58:57 +0300

    feat(express): add the new lessons

--- express-router/index.js
+++ express-router/index.js
@@ -1,26 +1,9 @@
 const express = require("express");
 const app = express();
-const router = express.Router();
 const port = 54321;
+const appRouter = require("./router/router");
 
-router.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
-router.post("/", (req, res) => {
-  res.json({ message: "basarili bir sekilde olusturuldu" }).status(201);
-});
-
-router.put("/", (req, res) => {
-  res.json({ message: "basarili bir sekilde guncellendi" }).status(200);
-});
-
-router.delete("/", (req, res) => {
-  res.json({ message: "basarili bir sekilde silindi" });
-});
-
-// Eger router ile uygulama calistirilacaksa bunu use ile kullanmaliyiz!
-app.use(router);
+app.use(appRouter);
 
 app.listen(port, () => {
   console.log(`http://localhost:${port} calismaktadir!`);
