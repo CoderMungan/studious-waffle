2a957df44c0638865ae437790ba0d09e932d8c6c
CoderMungan <codermungan@gmail.com>
2024-10-02 02:21:57 +0300

    feat(algorithms): add the algorithms

---
+++ javascript/algo.js
@@ -0,0 +1,36 @@
+// Palindrome algoritm
+function isPalindrome(str) {
+  let reversed = str.split("").reverse().join("");
+  return str == reversed;
+}
+// Fibonacci
+function fibonacci(n) {
+  let fib = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib;
+}
+
+//Factorial
+function factorial(n) {
+  if (n <= 0) {
+    return 1;
+  } else if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+// Anagram
+function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  return str1.split("").sort().join("") === str2.split("").sort().join("");
+}
+
+// PositiveNumberSum
+function positiveNumberSum(arr) {
+  return arr.filter((x) => x > 0).reduce((a, b) => a + b, 0);
+}
--- javascript/array.js
+++ javascript/array.js
@@ -25,25 +25,25 @@ console.log(secAar);
 // iki veya daha fazla diziyi birlestirmek icin concat metodu kullanilir. concat turkcede "birlestirmek" demektir.
 let combineArr = secAar.concat(thirArr);
 let moreCombineArr = secAar.concat(thirArr, forSlice);
-console.log("combined array: ", combineArr);
-console.log("birden fazla birlestirme:", moreCombineArr);
+console.log("combined array :", combineArr);
+console.log("birden fazla birlestirme :", moreCombineArr);
 
 // Dizinin belli bir kismini kopyalayarak yeni bir dizi dondurur. slice turkcede "kesmek" demektir.
 // Aralik verilince son index numarasini dahil etmez!
 let sliced = forSlice.slice(0, 3);
-console.log("sliced: ", sliced);
+console.log("sliced :", sliced);
 
 // Diziden eleman ekler veya siler. Orjinal diziyi degistirir.
 // Copy veride ekleme gozukmuyor. Ayrica cikarma yaptiktan sonra , ile eklenecek elemani koyabilirsin.
 // .splice("cikarilacak baslangic index","son index araligi","eklenecek eleman")
 let spliced = forSplice.splice(3, 3, "Halil");
-console.log("spliced: ", spliced);
+console.log("spliced :", spliced);
 
 // Dizinin elemanlarini birlestirip, tek bir string dondurur.
-console.log("join icin: ", forJoin.join(" "));
+console.log("join icin :", forJoin.join(" "));
 
 // Dizinin elemanlarini ters cevirir.
-console.log("reverse icin: ", forJoin.reverse());
+console.log("reverse icin :", forJoin.reverse());
 
 // Diziyi siralar.
 console.log("Siralama :", forJoin.sort());
@@ -51,8 +51,8 @@ console.log("Siralama :", forJoin.sort());
 // forEach() Dizinin elemanlarini donguye alir ve islem yapar.
 numArr.forEach((num) => {
   emptyArr.push(num * 2);
-  console.log("bos array", emptyArr);
-  console.log("forEachsonuc:", num * 2);
+  console.log("bos array :", emptyArr);
+  console.log("forEachsonuc :", num * 2);
 });
 
 let forEachNewArr = numArr.forEach((num) => {
@@ -66,7 +66,7 @@ console.log("map :", newArrMap);
 
 // filter() Dizinin elemanlarini filtreler. Bir dizi dondurur
 let filter = numArr.filter((filt) => filt > 3);
-console.log("filter", filter);
+console.log("filter :", filter);
 
 // reduce() dizi elemanlarini bir degere indirger.
 // reduce() iki tane parametre alir.
@@ -74,10 +74,31 @@ console.log("filter", filter);
 // 2.Parametre: el yani Element, eleman!
 // 1. Parametre islem sonrasi biriktirme yapan yerdir.
 let sumReduce = numArr.reduce((acc, el) => acc + el, 0);
-console.log("sumReduce: ", sumReduce);
+console.log("sumReduce :", sumReduce);
+
+// find() kosulu saglayan ilk elemani doner. Orjinal dizi de degistirilmez.
+let firstEven = numArr.find((a) => a % 2 === 0);
+console.log("find:", firstEven);
+
+// sort() arrayi siralar
+let sortbyArr = [1, 4, 5, 6, 3, 2];
+let sortbyChar = ["Halil", "Mungan", "Fatmanur", "Cebi"];
+let sortArr = sortbyArr.sort((a, b) => a - b);
+let sortforChar = sortbyChar.sort((a, b) => a.localeCompare(b));
+
+console.log("sort:", sortArr);
+console.log("char sort :", sortforChar); // ["Cebi", "Fatmanur", "Halil", "Mungan]
+
+// every() tum elemanlarin belli kosulu saglanip, saglanmadigini dondurur. True ya da false dondurur.
+// orginal arrayi degistirmez.
+let everyArr = [1, 2, 3, 4, 5];
+let everyTrueArray = [2, 4, 6];
+let allEven = everyArr.every((a) => a % 2 === 0); // False
+let allTrueEven = everyTrueArray.every((a) => a % 2 === 0); // True
+console.log("evet:", allEven);
+console.log("evet:", allTrueEven);
 
 // Algoritma reduce olayi.
-
 function sumArrayAlgo(arr) {
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
@@ -85,5 +106,4 @@ function sumArrayAlgo(arr) {
   }
   return sum;
 }
-
 console.log("sonuc", sumArrayAlgo([1, 2, 3, 4]));
